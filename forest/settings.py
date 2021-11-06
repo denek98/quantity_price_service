93d5112ee9f75eca95d0d15b1ab624c5cd2eaf33
denek98 <devimov1@gmail.com>
2021-11-06 20:13:34 +0200

    uploading files

--- forest/settings.py
+++ forest/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'import_export',
     'suppliers.apps.SuppliersConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
